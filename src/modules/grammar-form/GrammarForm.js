9ce08f08488f7cdffd24a75acdb16f6255fdf129
arthurhoch <arthur.j.h96@gmail.com>
2018-04-08 22:29:59 -0300

    Finish GrammarForm, just need componentization

--- src/modules/grammar-form/GrammarForm.js
+++ src/modules/grammar-form/GrammarForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Form, Icon, Input, Radio, Row, Col, Tag } from 'antd';
+import { Button, Form, Icon, Input, Radio, Row, Col, Tag, Select } from 'antd';
 
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
+const Option = Select.Option;
 const { Search, TextArea } = Input;
 
 class InputForm extends Component {
@@ -11,7 +12,12 @@ class InputForm extends Component {
     nonTerminal: '',
     terminal: '',
     nonTerminalList: [],
-    terminalList: []
+    terminalList: [],
+    productionsList: [],
+    production: {
+      nonTerminal: '',
+      terminalsList: []
+    }
   };
 
 
@@ -95,6 +101,91 @@ class InputForm extends Component {
     }
   }
 
+  //Other
+
+  terminalListOnChange = (e) => {
+    e.preventDefault();
+    let value = e.target.value;
+    if (!/[^a-zA-Z]/.test(value)) {
+      this.setState({
+        nonTerminal: value.charAt(value.length - 1).toUpperCase()
+      });
+    }
+  }
+
+  handleChange = (value) => {
+    console.log(`Selected: ${value}`);
+  }
+
+  createNewProduction = () => {
+    this.setState({
+      productionsList: [...this.state.productionsList, { terminalsList: [] }]
+    })
+  }
+
+  delProduction = (index) => {
+    console.log(index);
+    let productionsList = this.state.productionsList;
+    let a = productionsList.splice(index, 1);
+    console.log(a);
+
+    this.setState({
+      productionsList: productionsList
+    });
+    console.log(this.state.productionsList);
+  }
+
+  setNonTerminalProduction = (value, options) => {
+    let row = options.props.row;
+    let productionsList = this.state.productionsList;
+    productionsList[row].nonTerminal = value;
+    this.setState({
+      productionsList: productionsList
+    });
+  }
+
+  createNewTerminalProduction = (index) => {
+    let productionsList = this.state.productionsList;
+
+    productionsList[index].terminalsList.push('');
+    this.setState({
+      productionsList: productionsList
+    });
+  }
+
+  onSentenceChange = (e, index, indexTerminal) => {
+    let value = e.target.value;
+    let char = value.slice(-1);
+
+    let containsTerminal = this.state.terminalList.indexOf(char) > -1;
+    let containsNonTerminal = this.state.nonTerminalList.indexOf(char) > -1;
+
+    if (!/[^a-zA-Z]/.test(value) && (containsTerminal || containsNonTerminal)) {
+      console.log(index, indexTerminal);
+      let productionsList = this.state.productionsList;
+      console.log(productionsList[index]);
+      productionsList[index].terminalsList[indexTerminal] = value;
+
+      this.setState({
+        productionsList: productionsList
+      });
+    }
+  }
+
+  delSentence = (index, indexTerminal) => {
+    let productions = this.state.productionsList;
+    let terminalsList = productions[index].terminalsList;
+    terminalsList.splice(indexTerminal, 1);
+
+    this.setState({
+      productionsList: productions
+    });
+  }
+
+  terminalProductionOnChange = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div>
@@ -161,6 +252,78 @@ class InputForm extends Component {
               this.state.terminalList.length > 0 &&
               this.state.nonTerminalList.length > 0 &&
               <div>
+                <Button
+                  type="primary"
+                  onClick={this.createNewProduction}>
+                  Add production</Button>
+                {
+                  this.state.productionsList.map((production, index) =>
+                    <Row key={index}>
+                      <h1>{index}</h1>
+                      <Col md={5}>
+                        <Select
+                          showSearch
+                          style={{ width: 200 }}
+                          placeholder="Select a non-terminal"
+                          optionFilterProp="children"
+                          value={this.state.productionsList[index].nonTerminal}
+                          onSelect={this.setNonTerminalProduction}
+                          filterOption={(input, option) => option.props.children.toUpperCase().indexOf(input.toUpperCase()) >= 0}>
+                          {
+                            this.state.nonTerminalList.map((nt, indexNonterminal) =>
+                              <Option
+                                value={nt}
+                                row={index}
+                                key={indexNonterminal}>
+                                {nt}
+                              </Option>
+                            )
+                          }
+                        </Select>
+                      </Col>
+                      <Col md={5}>
+                        <h1>--></h1>
+                      </Col>
+                      <Col md={10}>
+                        {
+                          this.state.productionsList[index].terminalsList.map((terminal, indexTerminal) =>
+                            <div key={indexTerminal}>
+                              <Input
+                                key={indexTerminal}
+                                value={this.state.productionsList[index].terminalsList[indexTerminal]}
+                                onChange={e => this.onSentenceChange(e, index, indexTerminal)}
+                                placeholder="Sentence" />
+
+                              <Button
+                                type="danger"
+                                onClick={() => this.delSentence(index, indexTerminal)}>
+                                Del sentence
+                              </Button>
+                            </div>
+                          )}
+
+                        <Button
+                          type="primary"
+                          onClick={() => this.createNewTerminalProduction(index)}>
+                          Add sentence
+                        </Button>
+
+
+                      </Col>
+                      <Col md={5}>
+                        <Button
+                          type="danger"
+                          onClick={() => this.delProduction(index)}>
+                          Remove
+                        </Button>
+                      </Col>
+
+                    </Row>
+                  )}
+
+
+
+
                 <Row type="flex" justify="center" align="middle">
                   <Col span={12}>
                     <h4>Productions:</h4>
@@ -182,7 +345,6 @@ class InputForm extends Component {
     );
   }
 }
-
 
 const GrammarForm = Form.create()(InputForm)
 export default GrammarForm;
